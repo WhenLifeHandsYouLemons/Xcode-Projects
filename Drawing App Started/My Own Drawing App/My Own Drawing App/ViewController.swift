6aa2ceaa28de7adc082529d19be1d093a71f74ec
Sooraj Sannabhadti <2005sooraj@gmail.com>
2020-12-30 16:32:28 +0800

    Drawing App Started

---
+++ Drawing App Started/My Own Drawing App/My Own Drawing App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Own Drawing App
+//
+//  Created by suma sannabhadti on 7/30/20.
+//  Copyright © 2020 Sooraj Sannabhadti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Drawing App Started/My Own Drawing App/My Own Drawing AppTests/My_Own_Drawing_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_Own_Drawing_AppTests.swift
+//  My Own Drawing AppTests
+//
+//  Created by suma sannabhadti on 7/30/20.
+//  Copyright © 2020 Sooraj Sannabhadti. All rights reserved.
+//
+
+import XCTest
+@testable import My_Own_Drawing_App
+
+class My_Own_Drawing_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
